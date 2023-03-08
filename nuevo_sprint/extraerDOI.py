c28c6f68859f05f5526b6139aa97dbde1f910350
unknown <glp1002@alu.ubu.es>
2023-03-08 16:11:46 +0100

    Reorganización del repositorio

--- nuevo_sprint/extraerDOI.py
+++
@@ -1,4 +0,0 @@
-from habanero import Crossref
-cr = Crossref()
-result = cr.works(query = "Exploring the Dynamics of Quantum Entanglement in Multi-Particle Systems")
-print(result['message']['items'][0]['DOI']) # Salida: 10.1007/3-540-49208-9_1
