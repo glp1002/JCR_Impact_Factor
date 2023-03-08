a7f3c8b7018a02fc22594a3e52d4fc7e829b4e89
unknown <glp1002@alu.ubu.es>
2023-03-08 16:06:31 +0100

    [prototipo] Se actualiza el modelo de Crossref terminado

---
+++ extraccion_de_datos/modelo_crossref/crossref_prototipo.py
@@ -0,0 +1,246 @@
+"""
+Módulo que contiene las funciones de extracción de datos de la API de Crossref.
+
+============================
+  Trabajo de Fin de Grado
+Universidad de Burgos (UBU)
+============================
+
+Autor: Gadea Lucas Pérez
+Año: 2023
+"""
+
+import datetime
+import logging
+import re
+import csv
+
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Configuración del archivo de "log"
+logger = logging.getLogger('crossref_log')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('crossref.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.info('\nNew CROSSREF execution:')
+
+# Definición de métodos
+def journalExist(issn: str, journal: str) -> bool:
+    """
+    Verifica si una revista existe en la base de datos de Crossref.
+
+    Parameters:
+        issn (str): El identificador ISSN de la revista que se desea buscar en la base de datos de Crossref. 
+        journal (str): El nombre de la revista que deseas buscar en la base de datos de Crossref.
+
+    Returns:
+        bool: Devuelve True si la revista existe en la base de datos, de lo contrario devuelve False.
+
+    Ejemplo:
+        >>> journalExist('2326-3814','IEEE Transactions on Computers')
+        [START] Extracción de la revista IEEE Transactions on Computers
+        True
+        >>> journalExist('12345-12345','Nonexistent Journal')
+        [WARNING] La revista Nonexistent Journal no existe.
+        False
+    """
+        
+    # Especifica los parámetros de búsqueda de la API de Crossref
+    url = f"https://api.crossref.org/journals/{issn}"
+    
+    # Realiza una solicitud GET a la API de Crossref con los parámetros de búsqueda especificados
+    response = requests.get(url)
+    
+    # Si la solicitud es exitosa, se procede a extraer sus artículos 
+    if response.ok:
+        data = response.json()
+        
+        if 'title' in data['message']:
+            logger.info(f'Extraccion de la revista {journal}')
+            return True
+        else:
+            logger.warning(f'La revista {journal} no existe.')
+            return False
+    else:
+        logger.error(f'Error para "{journal}" al realizar la solicitud: {response.status_code}')
+        
+        
+def getArticles(issn: str, year_i: str, year_f: str) -> list[dict[str, str]]:
+    """
+    Obtiene todos los artículos publicados en una revista en un año determinado.
+
+    Parameters:
+        issn (str): El identificador de la revista que deseas buscar en la base de datos de Crossref.
+        year_i (str): El año inicial de publicación de los artículos que se desean buscar.
+        year_i (str): El año final de publicación de los artículos que se desean buscar.
+        
+    Returns:
+        List[Dict[str, str]]: Una lista de diccionarios de artículos que contienen los siguientes campos:
+        "DOI", "Título", "Autores", "Año", "Citas", "Revista".
+
+    """
+    articles = []
+    cursor = '*'
+    while cursor != '':
+        params = {
+            'filter': f'from-pub-date:{year_i}-01-01,until-pub-date:{year_f}-12-31',
+            'rows': 1000,
+            'cursor': cursor
+        }
+        response = requests.get(f'http://api.crossref.org/journals/{issn}/works', params=params)
+        if response.ok:
+            data = response.json()
+            items = data.get('message', {}).get('items', [])
+            for item in items:
+                article = {
+                    "DOI": item.get('DOI', ''),
+                    "Título": formatData(item.get('title', ['n.m.'])[0]),
+                    "Autores": formatData(', '.join(author.get('given', 'n.m.') + ' ' + author.get('family', 'n.m.') for author in item.get('author', []))),
+                    "Año": item.get('published-print', {}).get('date-parts', [["n.m."]])[0][0],
+                    "Citas": item.get('is-referenced-by-count', 0),
+                    "Revista": item.get('container-title', ["n.m."])[0]
+                }
+                articles.append(article)
+            if data['message']['total-results'] - len(articles) == 0:
+                cursor = ''
+            else:
+                cursor = data.get('message', {}).get('next-cursor', '')
+        else:
+            logger.error(f'Error al realizar la solicitud: {response.status_code}')
+            cursor = ''
+    return articles
+
+def formatData(data: str) -> str:
+    """
+    Formatea y limpia una cadena de texto eliminando espacios en blanco innecesarios y etiquetas HTML.
+
+    Args:
+        data (str): La cadena de texto que se debe formatear y limpiar.
+
+    Returns:
+        str: La cadena de texto formateada y limpia, sin espacios en blanco innecesarios ni etiquetas HTML.
+    """
+    
+    # Reemplazar las etiquetas HTML con una cadena vacía
+    format_data = re.sub('<.*?>', '', data)
+    # Eliminamos espacios en blanco y las retornos de línea
+    format_data = re.sub('\s\s*', ' ', format_data.strip()).replace('\n', '').replace('\r', '')
+    # Eliminamos las comas seguidas (campos vacíos)
+    format_data = format_data.replace(',,', ',n.m.,')
+    
+    return format_data
+
+def loadData(csv_name: str) -> list:
+    """
+    Carga los datos de un archivo CSV que contiene información sobre revistas científicas
+    y devuelve una lista de tuplas, donde cada tupla contiene el nombre del journal y su ISSN.
+
+    Args:
+        csv_name (str): El nombre del CSV que contiene la lista de revistas.
+
+    Returns:
+        list: Lista de tuplas que contiene el nombre del journal y su ISSN.
+    
+    """
+    journal_list = []
+
+    # Abrir el archivo CSV y leerlo
+    with open(csv_name, encoding='utf-8', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+
+        # Saltar las filas iniciales que no contienen información de revistas
+        for i in range(3):
+            next(reader)
+
+        # Iterar sobre las filas restantes y extraer el nombre del journal y el ISSN
+        for row in reader:
+            if len(row) > 1:
+                journal_list.append((row[0], row[3]))
+    return journal_list
+
+def plotBar(time_data: dict) -> None:
+    """
+    Crea una gráfica de barras a partir de un diccionario de tiempos de extracción de revistas.
+
+    Args:
+        time_data (dict): Un diccionario con los tiempos de extracción de cada revista.
+
+    Returns:
+        None: La función no retorna ningún valor.
+    """
+    if time_data != None and len(time_data) > 0:
+        if any(val < 0 for val in time_data.values()):
+            logger.error(f"No puede haber valores negativos en los tiempos de extraccion.")
+        else:
+            # Crear la gráfica de barras
+            plt.bar(time_data.keys(), time_data.values(), width=1)
+
+            # Personalizar la gráfica
+            plt.title("Tiempo de estracción en función de la revista")
+            plt.xlabel("ISSN de la revista")
+            plt.ylabel("Tiempo (segundos)")
+            plt.xticks(rotation=45)
+            plt.subplots_adjust(bottom=0.20)
+            
+            # Guardar la gráfica en un archivo de imagen
+            plt.savefig('grafica_tiempos.png')
+    else:
+        logger.warning(f'No hay datos para graficar.')
+
+
+
+
+def main(year_i:str, year_f:str, csv_name: str) -> int: 
+    
+    # Gráficas de tiempo 
+    time_data = {}
+    
+    # Especifica los parámetros de búsqueda de la API de Crossref
+    for (journal,issn) in loadData(csv_name):
+        
+        # Comprobamos que la revista en cuestión existe
+        if journalExist(issn, journal):
+
+            # Comprobamos el tiempo que tarda -> INICIO
+            initial_time = datetime.datetime.now()
+            logger.info(f"[TIME] La hora de comienzo es: {initial_time.time()}")
+
+            # Realizamos la búsqueda de los artículos (a partir de cualquiera de sus dos ISSN)
+            data = getArticles(issn, year_i, year_f)
+            
+            # Transformamos los datos a CSV
+            # NOTA: en un futuro irá directamente a la BBDD
+            df = pd.DataFrame(data)
+            df.to_csv('resultados_'+ issn +'.csv', index=False, encoding='utf-8')
+
+            # Comprobamos el tiempo que tarda -> FIN
+            final_time = datetime.datetime.now()
+            logger.info(f"[TIME] La hora de finalización es: {final_time.time()}")
+            logger.info(f"[TIME] El tiempo que ha tardado es: {final_time-initial_time}")
+            time_data[issn] = int(final_time.timestamp()) - int(initial_time.timestamp())
+            
+    # Generamos la gráfica de tiempo
+    plotBar(time_data)
+    
+   
+
+if __name__ == '__main__':
+    main()
+    
+   
+    
+
+
+
+
+
+
+
+
+
+
+
---
+++ extraccion_de_datos/modelo_crossref/crossref_test.py
@@ -0,0 +1,122 @@
+"""
+Módulo de pruebas para el modelo de extracción de datos de Crossref.
+
+============================
+  Trabajo de Fin de Grado
+Universidad de Burgos (UBU)
+============================
+
+Autor: Gadea Lucas Pérez
+Año: 2023
+"""
+
+import csv
+import os
+import tempfile
+import unittest
+
+import crossref_prototipo as cr
+
+class TestJournalExist(unittest.TestCase):
+    
+    def test_journal_exist(self):
+        # Caso de prueba 1: revista existe
+        self.assertTrue(cr.journalExist('2168-2267','IEEE Transactions on Big Data'))
+        
+        # Caso de prueba 2: revista no existe
+        self.assertFalse(cr.journalExist('12345-12345','Nonexistent Journal'))
+        
+    def test_load_data(self):
+        # Caso de prueba 3: Comprobar que se cargan bien los datos
+        # Crear un archivo CSV temporal con un registro de prueba
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+            writer = csv.writer(f)
+            writer.writerows([] for i in range(3))
+            writer.writerow(['Intelligent Decision Technologies-Netherlands', 'INTELL DECIS TECHNOL', '1872-4981', '1875-8843', 'COMPUTER SCIENCE, ARTIFICIAL INTELLIGENCE - ESCI', '210', 'N/A', 'N/A', '0.11', '0.71%'])
+        # Llamar a la función loadData() y comprobar que devuelve los datos correctos
+        data = cr.loadData(f.name)
+        self.assertEqual(data, [('Intelligent Decision Technologies-Netherlands', '1875-8843')])
+        # Eliminar el archivo CSV temporal
+        os.remove(f.name)
+        
+    def test_formatData(self):
+        # Caso de prueba 4: Comprobar que se eliminan bien las etiquetas HTML y los espacios
+        input_data = "<p>This is a  text with <em>html</em> tags and  extra spaces.  </p>"
+        expected_output = "This is a text with html tags and extra spaces."
+        self.assertEqual(cr.formatData(input_data), expected_output)
+        
+        # Caso de prueba 5: Comprobar que se eliminan bien las etiquetas "href"
+        input_data = "<p> <em> This is a </em> another text with extra  spaces and <a href='https://www.example.com'>links</a>. </p>"
+        expected_output = "This is a another text with extra spaces and links."
+        self.assertEqual(cr.formatData(input_data), expected_output)
+        
+        # Caso de prueba 6: Comprobar que los "strings" que no han de cambiar, no cambian      
+        input_data = "This is a text with no HTML tags or extra spaces."
+        expected_output = "This is a text with no HTML tags or extra spaces."
+        self.assertEqual(cr.formatData(input_data), expected_output)
+        
+        # Caso de prueba 7: Comprobar que no hay campos vacíos (rellenar con n.m.)  
+        input_data = "This is a text with,, some void ,, fields."
+        expected_output = "This is a text with,n.m., some void ,n.m., fields."
+        self.assertEqual(cr.formatData(input_data), expected_output) 
+        
+    def test_getArticles_returns_list_of_dicts(self):
+        # Caso de prueba 8: Devuelve una lista de artículos
+        articles = cr.getArticles('1533-7146', '2020', '2021')
+        self.assertIsInstance(articles, list)
+        for article in articles: 
+            self.assertIsInstance(article, dict)
+            
+    def test_getArticles_returns_valid_keys(self):
+        # Caso de prueba 9: Las "keys" o claves del diccionario son correctas
+        articles = cr.getArticles('1533-7146', '2020', '2021')
+        expected_keys = ["DOI", "Título", "Autores", "Año", "Citas", "Revista"]
+        for article in articles:
+            self.assertEqual(list(article.keys()), expected_keys)
+            
+    def test_getArticles_correct_article_count(self):
+        # Caso de prueba 10: Devuelve el número correcto de artículos
+        articles = cr.getArticles('1533-7146', '2020', '2021')
+        self.assertEqual(len(articles), 86)
+        
+    def test_getArticles_invalid_issn_raises_error(self):
+        # Caso de prueba 11: ISSN inválido
+        articles = cr.getArticles('invalid_issn', '2020', '2021')
+        self.assertEqual(len(articles), 0)
+        self.assertEqual(articles, [])  
+              
+    def test_getArticles_invalid_year(self):
+        # Caso de prueba 12: Año incorrecto -> orden incorrecto
+        articles = cr.getArticles('1533-7146', '2023', '2022')
+        self.assertEqual(len(articles), 0)
+        self.assertEqual(articles, [])  
+        
+        # Caso de prueba 13: Año incorrecto -> años inválidos
+        articles = cr.getArticles('1533-7146', '2016', '99999')
+        self.assertEqual(len(articles), 0)
+        self.assertEqual(articles, [])  
+        
+    def test_main(self):
+        # Ejecución completa del prototipo -> requisito: CSV de entrada de datos
+        cr.main("2000", "2023", 'JCR_JournalResults_01_2023_ComputerScience_AI.csv')
+
+        # Caso de prueba 14: Se genera una gráfica al finalizar el proceso
+        self.assertTrue(os.path.isfile('grafica_tiempos.png'))
+
+        # Caso de prueba 15: Se generan ficheros CSVs de resultados 
+        self.assertTrue(os.path.isfile('resultados_1435-5655.csv'))
+        self.assertTrue(os.path.isfile('resultados_1875-8827.csv'))
+    
+        # Caso de prueba 16: Se genera un fichero de log
+        self.assertTrue(os.path.isfile('crossref.log'))
+        
+        # Caso de prueba 17: La información de log es correcta
+        log_file = open("crossref.log", "r")
+        log_contents = log_file.read()
+        self.assertIn(' - crossref_log - ERROR - Error para "Foundations of Computing and Decision Sciences" al realizar la solicitud: 404', log_contents)
+        self.assertIn(' - crossref_log - ERROR - Error para "International Journal of Innovative Computing Information and Control" al realizar la solicitud: 404', log_contents)
+        self.assertIn(' - crossref_log - INFO - Extraccion de la revista Patterns', log_contents)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
