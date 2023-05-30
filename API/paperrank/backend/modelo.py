b7c8d656a1f56fd0b3ce7a35a5ec946028e70c87
unknown <glp1002@alu.ubu.es>
2023-05-30 21:25:39 +0200

    [Heroku] Avances

--- API/paperrank/backend/modelo.py
+++ API/paperrank/backend/modelo.py
@@ -33,8 +33,8 @@ class Modelo:
     def __init__(self):
         try:
             database_url = os.environ.get("DATABASE_URL")
-            if database_url and database_url.startswith("postgres://"):
-                database_url = database_url.replace("postgres://", "postgresql://", 1)
+            # if database_url and database_url.startswith("postgres://"):
+            #     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
             self.conn = psycopg2.connect( 
                 database_url, 
